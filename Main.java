597de62a248fa08e020b2eaa4de8b48d1607f406
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2022-07-13 14:16:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+//Classes:-
+// Follows PascelNaming Convention
+// ex: MyNameLol
+
+public class Main {
+  
+//Functions:-
+// Follows camleCaseNaming Convention
+// ex: myNameLol 
+
+  public static void main(String[] args) {
+   
+    System.out.println("Hello World");
+  }
+}
+
+//             Data Types:
+//   Non Primitive data type:- Derive from the primitve data type
